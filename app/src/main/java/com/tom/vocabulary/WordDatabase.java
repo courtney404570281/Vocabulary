62ccf5d33d8c56b19292b15d56387e02ea1084a7
Hank Tom <hanktom@gmail.com>
2019-07-27 13:42:53 +0800

    JSON

--- app/src/main/java/com/tom/vocabulary/WordDatabase.java
+++ app/src/main/java/com/tom/vocabulary/WordDatabase.java
@@ -2,13 +2,25 @@ package com.tom.vocabulary;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @Database(entities = {Word.class}, version = 1)
 public abstract class WordDatabase extends RoomDatabase {
     public abstract WordDao wordDao();
+
     private static volatile WordDatabase instance = null;
 
     public static WordDatabase getInstance(Context context) {
@@ -16,8 +28,43 @@ public abstract class WordDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context,
                     WordDatabase.class,
                     "word-database")
+                    .addCallback(new Callback() {
+                        @Override
+                        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                            super.onCreate(db);
+
+                        }
+                    })
                     .build();
+            fillDataFromJsonFile(instance.wordDao(), context);
         }
         return instance;
+    }
+
+    private static void fillDataFromJsonFile(WordDao wordDao, Context context) {
+        InputStream is = context.getResources()
+                .openRawResource(R.raw.vocabulary);
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is));
+        String line = null;
+        StringBuilder builder = new StringBuilder();
+        try {
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+            JSONObject json = new JSONObject(builder.toString());
+            JSONArray words = json.getJSONArray("words");
+            for (int i = 0; i < words.length(); i++) {
+                JSONObject word = words.getJSONObject(i);
+                Word w = new Word(word.getString("word"),
+                        word.getString("means"),
+                        word.getInt("star") == 0 ? false : true);
+                wordDao.insert(w);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
